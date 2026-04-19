387aeb6b4f4cbec5816fe466890073da0deb5ac0
Miguel <miguelbedmards@gmail.com>
2026-04-19 19:51:38 +0200

    Se han creado las tablas cliente , socio , producto y se comenzara a crear la tabla tienda

--- bin/bbdd/Game.sql
+++ bin/bbdd/Game.sql
@@ -4,3 +4,15 @@ CREATE DATABASE IF NOT EXISTS Game;
 USE Game;
 
 
+CREATE TABLE usuario(
+id_usuario INT  AUTO_INCREMENT PRIMARY KEY, 
+nombre VARCHAR(64),
+apellido VARCHAR (64),
+telefono VARCHAR(48),
+email VARCHAR(58),
+ciudad VARCHAR(48),
+fecha_nacimiento DATE
+);
+
+
+
--- src/bbdd/Game.sql
+++ src/bbdd/Game.sql
@@ -2,14 +2,38 @@ DROP DATABASE IF EXISTS Game;
 CREATE DATABASE IF NOT EXISTS Game;
 
 USE Game;
+CREATE TABLE Socio(
+id_socio INT AUTO_INCREMENT PRIMARY KEY,
+punto_game INT DEFAULT 0,
+fecha_registro DATE	
 
-CREATE TABLE usuario(
-id_usuario INT  AUTO_INCREMENT PRIMARY KEY, 
-nombre VARCHAR(64),
-apellido VARCHAR (64),
-telefono VARCHAR(48),
-email VARCHAR(58),
-ciudad VARCHAR(48),
-fecha_nacimiento DATE
 );
+
+CREATE TABLE Cliente(
+id_usuario INT  AUTO_INCREMENT PRIMARY KEY, 
+nombre VARCHAR(64) NOT NULL,
+apellido VARCHAR (64),
+telefono VARCHAR(48) NOT NULL ,
+email VARCHAR(58) NOT NULL UNIQUE	,
+ciudad VARCHAR(48),
+es_socio BOOLEAN,
+fecha_nacimiento DATE,
+id_socio INT,
+FOREIGN KEY(id_socio) REFERENCES Socio(id_socio)
+);
+
+CREATE TABLE Producto(
+id_producto INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR (64) NOT NULL,
+descripcion VARCHAR (64),
+precio DECIMAL (10,2) NOT NULL,
+plataforma VARCHAR (58),
+tipo ENUM('Videojuego','Consola','Accesorio','PC') NOT NULL
+);
+CREATE TABLE Tienda(
+id_tienda INT AUTO_INCREMENT PRIMARY KEY
+
+
+);
+
 
