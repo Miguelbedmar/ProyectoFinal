ddd8ea377680004125241c1adc4999601a9763b7
Miguel <miguelbedmards@gmail.com>
2026-04-19 20:13:42 +0200

    Se ha termina la base de datos se esperan cambios

--- src/bbdd/Game.sql
+++ src/bbdd/Game.sql
@@ -10,7 +10,7 @@ fecha_registro DATE
 );
 
 CREATE TABLE Cliente(
-id_usuario INT  AUTO_INCREMENT PRIMARY KEY, 
+id_cliente INT  AUTO_INCREMENT PRIMARY KEY, 
 nombre VARCHAR(64) NOT NULL,
 apellido VARCHAR (64),
 telefono VARCHAR(48) NOT NULL ,
@@ -31,9 +31,35 @@ plataforma VARCHAR (58),
 tipo ENUM('Videojuego','Consola','Accesorio','PC') NOT NULL
 );
 CREATE TABLE Tienda(
-id_tienda INT AUTO_INCREMENT PRIMARY KEY
+id_tienda INT AUTO_INCREMENT PRIMARY KEY,
+id_cliente INT,
+FOREIGN KEY(id_cliente)REFERENCES Cliente (id_cliente),
+ciudad VARCHAR(64)
+);
+CREATE TABLE Venta(
+id_venta INT AUTO_INCREMENT PRIMARY KEY,
+id_cliente INT,
+id_tienda INT,
+FOREIGN KEY(id_cliente)REFERENCES Cliente (id_cliente),
+FOREIGN KEY(id_tienda)REFERENCES Tienda (id_tienda)
+);
 
-
+CREATE TABLE Almacen(
+id_almacen INT AUTO_INCREMENT PRIMARY KEY,
+id_producto INT,
+id_tienda INT,
+stock_almacen INT,
+FOREIGN KEY (id_producto) REFERENCES Producto (id_producto),
+FOREIGN KEY (id_tienda) REFERENCES Tienda (id_tienda)
+);
+CREATE TABLE Detalle_Venta(
+id_detalle_venta INT AUTO_INCREMENT PRIMARY KEY ,
+id_venta INT,
+id_producto INT,
+precio_unidad DECIMAL (10,2),
+cantidad INT,
+FOREIGN KEY (id_venta) REFERENCES Venta(id_venta),
+FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
 );
 
 
