41d2c048b6d46da5087d1b30f8e79c9034348e66
Miguel <miguelbedmards@gmail.com>
2026-04-20 18:18:47 +0200

    Se ha terminado la base de datos

--- bin/bbdd/Game.sql
+++ bin/bbdd/Game.sql
@@ -2,17 +2,70 @@ DROP DATABASE IF EXISTS Game;
 CREATE DATABASE IF NOT EXISTS Game;
 
 USE Game;
+CREATE TABLE Socio(
+id_socio INT AUTO_INCREMENT PRIMARY KEY,
+punto_game INT DEFAULT 0,
+fecha_registro DATE	
 
-
-CREATE TABLE usuario(
-id_usuario INT  AUTO_INCREMENT PRIMARY KEY, 
-nombre VARCHAR(64),
-apellido VARCHAR (64),
-telefono VARCHAR(48),
-email VARCHAR(58),
-ciudad VARCHAR(48),
-fecha_nacimiento DATE
 );
 
+CREATE TABLE Cliente(
+id_cliente INT  AUTO_INCREMENT PRIMARY KEY, 
+nombre VARCHAR(64) NOT NULL,
+apellido VARCHAR (64),
+telefono VARCHAR(48) NOT NULL ,
+email VARCHAR(58) NOT NULL UNIQUE	,
+ciudad VARCHAR(48),
+es_socio BOOLEAN,
+fecha_nacimiento DATE,
+id_socio INT,
+FOREIGN KEY(id_socio) REFERENCES Socio(id_socio)
+);
+
+CREATE TABLE Producto(
+id_producto INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR (64) NOT NULL,
+descripcion VARCHAR (64),
+precio DECIMAL (10,2) NOT NULL,
+plataforma VARCHAR (58),
+tipo ENUM('Videojuego','Consola','Accesorio','PC') NOT NULL
+);
+CREATE TABLE Tienda(
+id_tienda INT AUTO_INCREMENT PRIMARY KEY,
+ciudad VARCHAR(64)
+);
+
+CREATE TABLE Cliente_Tienda(
+id_cliente INT,
+id_tienda INT,
+FOREIGN KEY (id_cliente)REFERENCES Cliente (id_cliente),
+FOREIGN KEY (id_tienda)REFERENCES Tienda (id_tienda)
+);
+CREATE TABLE Venta(
+id_venta INT AUTO_INCREMENT PRIMARY KEY,
+fecha_venta DATE NOT NULL,
+id_cliente INT,
+id_tienda INT,
+FOREIGN KEY(id_cliente)REFERENCES Cliente (id_cliente),
+FOREIGN KEY(id_tienda)REFERENCES Tienda (id_tienda)
+);
+
+CREATE TABLE Almacen(
+id_almacen INT AUTO_INCREMENT PRIMARY KEY,
+id_producto INT,
+id_tienda INT,
+stock_almacen INT,
+FOREIGN KEY (id_producto) REFERENCES Producto (id_producto),
+FOREIGN KEY (id_tienda) REFERENCES Tienda (id_tienda)
+);
+CREATE TABLE Detalle_Venta(
+id_detalle_venta INT AUTO_INCREMENT PRIMARY KEY ,
+id_venta INT,
+id_producto INT,
+precio_unidad DECIMAL (10,2),
+cantidad INT,
+FOREIGN KEY (id_venta) REFERENCES Venta(id_venta),
+FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
+);
 
 
