2bd328ecc4f5afdbf2032fe751c94bcf12754395
Miguel <miguelbedmards@gmail.com>
2026-04-21 19:36:22 +0200

    Se ha terminado la plantilla productos

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,5 +1,77 @@
 package modelo;
 
 public class Producto {
+	// ATRIBUTO
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	private String plataforma;
+	private tipoProducto tipo;
+
+	private enum tipoProducto {
+		VIDEOJUEGO, CONSOLA, ACCESORIO, PC;
+	}
+
+	// CONSTRUCTOR
+	public Producto(String nombre, String descripcion, double precio, String plataforma, tipoProducto tipo) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.plataforma = plataforma;
+		this.tipo = tipo;
+
+	}
+
+	// GETTERS Y SETTERS
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public tipoProducto getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(tipoProducto tipo) {
+		this.tipo = tipo;
+	}
+	
 
 }
