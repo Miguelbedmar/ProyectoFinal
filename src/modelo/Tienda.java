df5f9010e84782d64d8686a94fbf97ed615f0b5a
Miguel <miguelbedmards@gmail.com>
2026-04-23 08:23:10 +0200

    Se ha terminado los modelos tienda y venta

--- src/modelo/Tienda.java
+++ src/modelo/Tienda.java
@@ -2,4 +2,30 @@ package modelo;
 
 public class Tienda {
 
+	// ATRIBUTOS
+	private int id;
+	private String ciudad;
+
+	// CONSTRUCTOR
+	public Tienda(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	// GETTERS Y SETTERS
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
 }
--- src/modelo/Venta.java
+++ src/modelo/Venta.java
@@ -1,5 +1,52 @@
 package modelo;
 
+import java.util.Date;
+
 public class Venta {
+	// ATRIBUTOS
+	protected int idVenta;
+	protected Date fechaVenta;
+	protected Cliente cliente;
+	protected Tienda tienda;
+
+	// CONSTRUCTOR
+	public Venta(Date fechaVenta, Cliente cliente, Tienda tienda) {
+		this.fechaVenta = fechaVenta;
+		this.cliente = cliente;
+		this.tienda = tienda;
+	}
+
+	// GETTERS Y SETTERS
+	public int getIdVenta() {
+		return idVenta;
+	}
+
+	public void setIdVenta(int idVenta) {
+		this.idVenta = idVenta;
+	}
+
+	public Date getFechaVenta() {
+		return fechaVenta;
+	}
+
+	public void setFechaVenta(Date fechaVenta) {
+		this.fechaVenta = fechaVenta;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Tienda getTienda() {
+		return tienda;
+	}
+
+	public void setTienda(Tienda tienda) {
+		this.tienda = tienda;
+	}
 
 }
