227daa22c5a5a544936f76054fe61ebcccd4e287
Miguel <miguelbedmards@gmail.com>
2026-04-24 11:33:29 +0200

    Se ha comenzado la plantilla productoDAO

---
+++ src/conexion/ProductoDao.java
@@ -0,0 +1,44 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Producto;
+
+public class ProductoDao {
+
+	private Connection conexion;
+
+	public ProductoDao() {
+		conexion = Conexion.getInstance().getConexion();
+	}
+
+	public ArrayList<Producto> obtenerTodosProductos() throws SQLException {
+		List<Producto> producto = new ArrayList<>();
+
+		String sql = "SELECT * FROM Producto";
+
+		try (PreparedStatement ps = conexion.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
+			while (rs.next()) {
+			Producto p=new Producto(
+				rs.getString("nombre"),
+				rs.getString("descripcion"),	
+				rs.getDouble("precio"),
+				rs.getString("plataforma"),
+					);	
+			producto.add(p);
+				
+			
+			}
+
+		}
+		return (ArrayList<Producto>)producto;
+
+	}
+
+}
