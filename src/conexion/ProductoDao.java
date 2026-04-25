8d4e98b8ed61f409c8c3b7d05283278d5f3d34cf
Miguel <miguelbedmards@gmail.com>
2026-04-25 13:19:21 +0200

    Se ha terminado el metodo de buscar producto

--- src/conexion/ProductoDao.java
+++ src/conexion/ProductoDao.java
@@ -7,8 +7,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import modelo.Almacen;
+import modelo.DetalleVenta;
 import modelo.Producto;
 import modelo.Producto.tipoProducto;
+import modelo.Venta;
 
 public class ProductoDao {
 
@@ -23,26 +26,52 @@ public class ProductoDao {
 
 		String sql = "SELECT * FROM Producto";
 
-		try (PreparedStatement ps = conexion.prepareStatement(sql); 
-				ResultSet rs = ps.executeQuery()) {
-		
+		try (PreparedStatement ps = conexion.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
 			while (rs.next()) {
-		
-				String tipoStr=rs.getString("tipo"); // convertir un tipo String a un enum
-				tipoProducto tipo=tipoProducto.valueOf(tipoStr);
-			Producto p=new Producto(
-				rs.getString("nombre"),
-				rs.getString("descripcion"),	
-				rs.getDouble("precio"),
-				rs.getString("plataforma"),
-				tipo
-					);	
-			producto.add(p);
-			
+				/*
+				 * La linea 35 recoge un tipo string proveniente de la base de datos de
+				 * categoria enum la linea 36 se encarga de buscar tipoProducto que es el nombre
+				 * del enum , una vez que haya encontrado el enum este lo pasa por un value.Of
+				 * que transforma el string a un valor de tipo enum.
+				 */
+				String tipoStr = rs.getString("tipo");
+				tipoProducto tipo = tipoProducto.valueOf(tipoStr);
+				Producto p = new Producto(rs.getString("nombre"), rs.getString("descripcion"), rs.getDouble("precio"),
+						rs.getString("plataforma"), tipo);
+				producto.add(p);
+
 			}
 
 		}
-		return (ArrayList<Producto>)producto;
+		return (ArrayList<Producto>) producto;
+
+	}
+
+	public ArrayList<Producto> buscarProducto(String nombre) throws SQLException {
+		List<Producto> producto = new ArrayList<>();
+
+		String sql = "SELECT * FROM Producto WHERE nombre LIKE ?";
+		try (PreparedStatement ps = conexion.prepareStatement(sql);) {
+			ps.setString(1, "%" + nombre + "%");
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				String tipoStr = rs.getString("tipo");
+
+				tipoProducto tipo = tipoProducto.valueOf(tipoStr);
+
+				Producto p = new Producto(rs.getString("nombre"), rs.getString("descripcion"), rs.getDouble("precio"),
+						rs.getString("plataforma"), tipo);
+				producto.add(p);
+			}
+		}
+
+		return (ArrayList<Producto>) producto;
+	}
+
+	public void realizarCompra(Venta venta, DetalleVenta detalleVenta, Almacen almacen, Producto producto) {
 
 	}
 
