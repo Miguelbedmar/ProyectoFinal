09e39e23a986e01249cc0c840523802cf3671636
Miguel <miguelbedmards@gmail.com>
2026-05-09 16:46:21 +0200

    Se ha creado la ventana principal

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -4,4 +4,20 @@ import javax.swing.JFrame;
 
 public class VentanaPrincipal extends JFrame {
 
+	public VentanaPrincipal() {
+		setTitle("Login ");
+		setSize(500,750);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	
+	
+
+	
+	
+	
+	
 }
